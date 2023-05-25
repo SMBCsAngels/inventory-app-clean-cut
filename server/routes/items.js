ec896ff9add25a5299bb040c7d5a96aba23165f5
Meah Game <meah.game@hotmail.co.uk>
2023-05-25 15:42:34 +0100

    thursday -get1

--- server/routes/items.js
+++ server/routes/items.js
@@ -8,11 +8,24 @@ const { Item } = require("../models");
 router.get("/", async (req, res, next) => {
     try {
         const items = await Item.findAll();
-        console.log(items)
         res.send(items);
     } catch(error) {
         next(error);
     }
 });
+router.get('/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        const item = await Item.findByPk(itemId);
+        if (!item) {
+            return res.status("Item not found");
+        }
+        res.json(item);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
 
 module.exports = router;
